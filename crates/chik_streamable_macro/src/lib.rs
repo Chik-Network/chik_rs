bf6a649f1cf43f76fc46e11fdabb74fa04d0b161
Chik-Network <Chik-Network@users.noreply.github.com>
2025-03-05 10:35:17 +0800

    update 0.21.0

--- crates/chik_streamable_macro/src/lib.rs
+++ crates/chik_streamable_macro/src/lib.rs
@@ -210,7 +210,7 @@ pub fn chik_streamable_macro(input: TokenStream) -> TokenStream {
                 }
             }
         },
-    };
+    }
 
     if !fnames.is_empty() {
         let ret = quote! {
